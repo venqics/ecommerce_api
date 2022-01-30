4002d311c4c0de936bc02ac56c02f4bbb27d85c3
Venkatesh <venx.rao87@gmail.com>
2022-01-30 20:43:38 +0530

    customizing and extending the user model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,7 @@
+from .models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    pass
