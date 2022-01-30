e58d5b8212ff8f6b6af339bb16bf620d2c42b206
Venkatesh <venx.rao87@gmail.com>
2022-01-30 20:05:46 +0530

    Configured and connected a postgres database

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+
+import os
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,11 +77,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+            "default": {
+                "ENGINE": "django.db.backends.postgresql_psycopg2",
+                "HOST" : config('DB_HOST', default='localhost'),
+                "NAME": config('DB_NAME'),
+                "USER": config('DB_USER'),
+                "PASSWORD": config('DB_PASSWORD'),
+                "PORT": "5432",
+            }
+ 
+ }
 
 
 # Password validation
